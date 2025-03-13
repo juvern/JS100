58d0fd637c0cea8967125783b560031c358db4bc
Juvern <juvern@see.com>
2025-03-13 16:04:29 +0000

    First commit

---
+++ variables/age.js
@@ -0,0 +1,11 @@
+let age = 20;
+let tenYears = 10;
+let twentyYears = 20;
+let thirtyYears = 30;
+let fortyYears = 40;
+
+console.log(`You are ${age} years old.`)
+console.log(`In ${tenYears} years, you will be ${age + tenYears} years old.`);
+console.log(`In ${twentyYears} years, you will be ${age + twentyYears} years old.`);
+console.log(`In ${thirtyYears} years, you will be ${age + thirtyYears} years old.`);
+console.log(`In ${fortyYears} years, you will be ${age + fortyYears} years old.`);
